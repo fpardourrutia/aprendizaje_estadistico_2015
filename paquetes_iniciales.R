8311c899e98331f1198ea41a69f4b3db69b9607d
Felipe Gonzalez <felipexgonzalez@gmail.com>
2015-07-28 13:07:08 -0500

    Agregar paquetes iniciales

---
+++ paquetes_iniciales.R
@@ -0,0 +1,8 @@
+options("repos"="http://cran.itam.mx")
+
+paquetes = c("devtools", "ggplot2", "dplyr", "tidyr", 
+	"stringr", "readr",  "readxl", "lubridate",
+	"glmnet",  "caret", "rpart", "randomForest", "gbm", "ElemStatLearn", "kknn",
+	"e1071", "nnet", "ROCR", "arm", "knitr", "rpart.plot")
+install.packages(paquetes)
+
