3f1f458dca20e51b30cf514f5f5c660973394f20
Felipe Gonzalez <felipexgonzalez@gmail.com>
2015-07-29 02:14:53 -0500

    Primera versión de clase 1

--- paquetes_iniciales.R
+++ paquetes_iniciales.R
@@ -3,6 +3,6 @@ options("repos"="http://cran.itam.mx")
 paquetes = c("devtools", "ggplot2", "dplyr", "tidyr", 
 	"stringr", "readr",  "readxl", "lubridate",
 	"glmnet",  "caret", "rpart", "randomForest", "gbm", "ElemStatLearn", "kknn",
-	"e1071", "nnet", "ROCR", "arm", "knitr", "rpart.plot")
+	"e1071", "nnet", "ROCR", "arm", "knitr", "rpart.plot","tsne")
 install.packages(paquetes)
 
