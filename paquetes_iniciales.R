bc7282ab90cfc1751338a1b6f7f7cbb0166bc674
Felipe Gonzalez <felipexgonzalez@gmail.com>
2015-07-30 19:30:37 -0500

    Agregar diagrammer a paquetes iniciales

--- paquetes_iniciales.R
+++ paquetes_iniciales.R
@@ -3,6 +3,7 @@ options("repos"="http://cran.itam.mx")
 paquetes = c("devtools", "ggplot2", "dplyr", "tidyr", 
 	"stringr", "readr",  "readxl", "lubridate",
 	"glmnet",  "caret", "rpart", "randomForest", "gbm", "ElemStatLearn", "kknn",
-	"e1071", "nnet", "ROCR", "arm", "knitr", "rpart.plot","tsne")
+	"e1071", "nnet", "ROCR", "arm", "knitr", "rpart.plot","tsne",
+	"DiagrammeR")
 install.packages(paquetes)
 
